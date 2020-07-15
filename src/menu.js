bd1ba1328de351e7437421473c7cec2e4e09e137
Marquis Bentley <livefreekai@icloud.com>
2020-07-14 21:47:19 -0700

    Create a menu class and ingredient class

---
+++ src/menu.js
@@ -0,0 +1,58 @@
+export const INGREDIENTS = {
+    // key: image url
+    'avacado': '../assets/ingredients/blk-avacado.PNG',
+    'beans': '../assets/ingredients/blk-beans.png',
+    'cheese': '../assets/ingredients/blk-cheese.png',
+    'ham': '../assets/ingredients/blk-ham.png',
+    'onion': '../assets/ingredients/blk-onion.png',
+    'pepper': '../assets/ingredients/blk-pepper.png',
+    'steak': '../assets/ingredients/blk-steak.png',
+    'tomato': '../assets/ingredients/blk-tomato.png',
+};
+
+export class Ingredient {
+    constructor(key, imageURL) {
+        this.key = key;
+        this.imageURL = imageURL;
+    }
+
+    //assign an image sprite to the ingredient
+    render() {
+        const img = document.createElement("img");
+        img.src = this.imageURL;
+        img.alt = `${this.key}-thumbnail`;
+        return img;
+    }
+}
+
+export class Menu {
+    constructor() {
+        this.menu = [];
+        this.generateMenu();
+    }
+
+    // collect a list of node elements with the class .ingredient, then create a
+    // new Ingredient instance for each node and add it that to the menu array.
+    // Finally, append the ingredient image to it's corresponding element 
+    generateMenu() {
+        const nodeList = document.querySelectorAll('.menu-item');
+        const nodeArr = Array.from(nodeList);
+
+        nodeArr.forEach( node => {
+            const key = node.id;
+            const ingredient = new Ingredient(key, INGREDIENTS[key]);
+            this.menu.push(ingredient);
+            node.append(ingredient.render());
+        });
+    }
+
+    // clear all the image tags attached to the menu so it can be randomized for 
+    // the next game
+    deleteMenu() {
+        const menu = document.querySelector('.menu');
+        const menuImgs = document.getElementsByTagName('img');
+        menuImgs.forEach(img => img.remove());
+
+        document.querySelector('modal').classList.add('hidden');
+    }
+}
---
+++ src/order.js
@@ -0,0 +1,10 @@
+export const ORDER_ITEMS = {
+    'avacado': '../assets/ingredients/avacado.PNG',
+    'beans': '../assets/ingredients/beans.png',
+    'cheese': '../assets/ingredients/cheese.png',
+    'ham': '../assets/ingredients/ham.png',
+    'onion': '../assets/ingredients/onion.png',
+    'pepper': '../assets/ingredients/pepper.png',
+    'steak': '../assets/ingredients/steak.png',
+    'tomato': '../assets/ingredients/tomato.png',
+};
