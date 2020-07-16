e3bbce73bcfecb78f1a97081c6be81efa3594f3d
Marquis Bentley <livefreekai@icloud.com>
2020-07-15 20:40:11 -0700

    Add icons for links and volume

--- src/customer.js
+++ src/customer.js
@@ -1,6 +1,15 @@
 class Customer {
     constructor(id, imageURL) {
         this.id = id;
-        this.imageURL = imageURL;
+        this.img = new Image();
+        this.img.src = imageURL;
+        this.img.onload = this.init;
+
+        this.canvas = document.querySelector('.canvas');
+        this.ctx = this.canvas.getContext('2d');
+    }
+
+    init() {
+        
     }
 }
--- src/order.js
+++ src/order.js
@@ -1,4 +1,4 @@
-export const ORDER_ITEMS = {
+export const OPTIONS = {
     'avacado': '../assets/ingredients/avacado.PNG',
     'beans': '../assets/ingredients/beans.png',
     'cheese': '../assets/ingredients/cheese.png',
@@ -10,5 +10,30 @@ export const ORDER_ITEMS = {
 };
 
 export const CUSTOMERS = {
-    1: '../assets/characters/blue'
+    1: '../assets/characters/blue-spritesheet.png',
+    2: '../assets/characters/purple-spritesheet.png',
+    3: '../assets/characters/white-spritesheet.png',
+    4: '../assets/characters/yellow-spritesheet.png'
+}
+
+export class Order {
+    constructor(orderSize, duration) {
+        this.orderSize = orderSize;
+        this.duration = duration;
+        this.order = [];
+
+        this.generateCustomer();
+        this.generateSpeechBubble();
+        this.generateOrder();
+        this.renderOrder();
+    }
+
+    generateOrder() {
+        const options = Object.keys(OPTIONS);
+        const numOptions = options.length;
+
+        for (i = 1; i <= this.numItems; i++) {
+            const idx = Math.floor(Math.random() * 5)
+        }
+    }
 }
