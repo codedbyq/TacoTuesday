903e3cf8a152639d7441ddd96de0a85bcd0365a8
Marquis Bentley <livefreekai@icloud.com>
2020-08-09 20:32:23 -0700

    Remove redundant code from menu class

--- src/menu.js
+++ src/menu.js
@@ -1,25 +1,3 @@
-// export const INGREDIENTS = {
-//   // key: image url
-//   avacado:
-//     "/assets/ingredients/blk-avacado.PNG",
-//   beans:
-//     "/assets/ingredients/blk-beans.png",
-//   cheese:
-//     "/assets/ingredients/blk-cheese.png",
-//   ham:
-//     "/assets/ingredients/blk-ham.png",
-//   onion:
-//     "/assets/ingredients/blk-onion.png",
-//   pepper:
-//     "/assets/ingredients/blk-pepper.png",
-//   steak:
-//     "/assets/ingredients/blk-steak.png",
-//   tomato:
-//     "/assets/ingredients/blk-tomato.png",
-//   rice:
-//     "/assets/ingredients/blk-rice.PNG",
-// };
-
 export const INGREDIENTS = {
   // key: image url
   avacado:
@@ -42,24 +20,8 @@ export const INGREDIENTS = {
     "https://taco-tuesday.s3-us-west-1.amazonaws.com/Ingredients/blk-rice.PNG",
 };
 
-// export class Ingredient {
-//     constructor(key, imageURL) {
-//         this.key = key;
-//         this.imageURL = imageURL;
-//     }
-
-//     // return an image sprite for the ingredient
-//     createImage() {
-//         const img = document.createElement("img");
-//         img.src = this.imageURL;
-//         img.alt = `${this.key}`;
-//         return img;
-//     }
-// }
-
 export class Menu {
   constructor() {
-      // this.menu = [];
       this.generateMenu();
   }
 
@@ -72,25 +34,15 @@ export class Menu {
     nodeArr.forEach( node => {
         const key = node.id;
         const ingredient = this.createImage(key, INGREDIENTS[key]);
-        // this.menu.push(ingredient);
         node.append(ingredient);
     });
   }
 
+  // Return an image html element with the given image url 
   createImage(key, imageURL) {
     const img = document.createElement("img");
     img.src = imageURL;
     img.alt = `${key}`;
     return img;
-  }
-
-  // clear all the image tags attached to the menu so it can be randomized for 
-  // the next game
-  deleteMenu() {
-      const menu = document.querySelector('.menu');
-      const menuImgs = document.getElementsByTagName('img');
-      menuImgs.forEach(img => img.remove());
-
-      document.querySelector('gameover').classList.add('hidden');
   }
 }
